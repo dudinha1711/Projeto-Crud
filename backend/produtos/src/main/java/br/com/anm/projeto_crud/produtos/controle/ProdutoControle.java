4f4fc2c00cdf35befdf0db7060f96fcea29be2eb
Maria Eduarda <duda.desenvolvedor@gmail.com>
2025-04-03 21:51:24 -0300

    Criação de método e rota cadastrar

--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
@@ -1,7 +1,10 @@
 package br.com.anm.projeto_crud.produtos.controle;
  
  import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.http.ResponseEntity;
  import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.PostMapping;
+ import org.springframework.web.bind.annotation.RequestBody;
  import org.springframework.web.bind.annotation.RestController;
  
  import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
@@ -13,11 +16,16 @@ package br.com.anm.projeto_crud.produtos.controle;
      @Autowired
      private ProdutoServico ps;
  
+     @PostMapping("/cadastrar")
+     public ResponseEntity<?> cadastar(@RequestBody ProdutoModelo pm){
+         return ps.cadastrar(pm);
+     }
+ 
      @GetMapping("/listar")
      public Iterable<ProdutoModelo> listar(){
          return ps.listar();
      }
-     
+ 
      @GetMapping("/")
      public String rota(){
          return "A API está funcionando!";
