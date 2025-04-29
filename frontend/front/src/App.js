8736cedbd69978b93f9a043b1cd769ba0a4f08e4
Maria Eduarda <duda.desenvolvedor@gmail.com>
2025-04-29 08:31:05 -0300

    Front vinculado ao Back

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
@@ -6,12 +6,23 @@ import Tabela from './Tabela';
 function App() {
 
   const[btnCadastrar] =useState(true);
+  const[produtos, setProdutos] =useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  },[]);
+
   return (
     <div>
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar}/>
-      <Tabela />
+      <Tabela vetor={produtos}/>
     </div>
   );
 }
 
 export default App;
+
+
