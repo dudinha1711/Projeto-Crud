283fae762ff6d0b63b3fef6cb8c4c43f604d62d1
Maria Eduarda <duda.desenvolvedor@gmail.com>
2025-04-29 11:35:46 -0300

    Estilização da tabela

--- frontend/front/src/Tabela.js
+++ frontend/front/src/Tabela.js
@@ -8,19 +8,27 @@ function Tabela({vetor}){
                     <th>Marca</th>
                     <th>Selecionar</th>
                 </tr>
-                <tbody>
-                    {
-                        vetor.map((obj,indice) => (
-                    <tr key={indice}>
-                        <td>{indice + 1}</td>
-                        <td>{obj.nome}</td>
-                        <td>{obj.marca}</td>
-                        <td><button className="btn btn-cuccess"></button></td>
-                    </tr>
-                        ))
-}
-                </tbody>
             </thead>
+           
+            <tbody>
+                
+                {
+                    
+                        vetor.map((obj,indice) => (
+
+
+                        <tr key={indice}>
+                           
+                            <td>{indice + 1}</td>
+                            <td>{obj.nome}</td>
+                            <td>{obj.marca}</td>
+                            <td><button className="btn btn-success">Selecionar</button></td>
+                        </tr>
+                         
+                ))
+            }
+            </tbody>
+           
         </table>
     )
 }
