0159be49dfaf778963cce71ac200078b6a9c91e4
Maria Eduarda <duda.desenvolvedor@gmail.com>
2025-05-27 19:56:38 -0300

    correção do codigo e implementação dos botões de cancelar, alterar e deletar

--- frontend/front/src/Formulario.js
+++ frontend/front/src/Formulario.js
@@ -1,20 +1,24 @@
-function Formulario({botao}){
-    return (
+function Formulario({botao, eventoTeclado, cadastrar, obj, cancelar, remover, alterar}){
+    return(
         <form>
-            <h1>Formulário</h1>
-            <input className="form-control" type="text" placeholder="Nome"/>
-            <input className="form-control" type="text" placeholder="Marca"/>
+
+            <h2>Sistema Gestor de Produtos</h2>
+
+            <input type="text" value={obj.nome} placeholder="Nome" onChange={eventoTeclado} name="nome" className="form-control"/>
+            <input type="text" value={obj.marca} placeholder="Marca" onChange={eventoTeclado} name="marca" className="form-control"/>
+
             {
-            botao
-            ?
-            <input className="btn btn-success" type="button" value="Cadastrar"/>
-            :
-            <div>
-                <input className="btn btn-primary" type="button" value="Alterar"/>
-                <input className="btn btn-danger" type="button" value="Remover"/>
-                <input className="btn btn-warning" type="button" value="Cancelar"/>
-            </div>
-        }
+               botao
+                ?
+                <input className="btn btn-success" type="button" value="Cadastrar" onClick={cadastrar}/>
+                :
+                <div>
+                    <input className="btn btn-warning" type="button" onClick={cancelar} value="Cancelar" />
+                    <input className="btn btn-primary" type="button" onClick={alterar} value="Alterar" />
+                    <input className="btn btn-danger"  type="button" onClick={remover} value="Remover" />
+                </div>
+            }
+            
         </form>
     )
 }
--- frontend/front/src/Tabela.js
+++ frontend/front/src/Tabela.js
@@ -1,33 +1,26 @@
-function Tabela({vetor}){
+function Tabela({vetor, selecionar}){
     return(
         <table className="table">
             <thead>
                 <tr>
-                    <th>#</th>
+                    <th>Código</th>
                     <th>Nome</th>
                     <th>Marca</th>
                     <th>Selecionar</th>
                 </tr>
-            </thead>
-           
-            <tbody>
-                
-                {
-                    
-                        vetor.map((obj,indice) => (
-
-
+                </thead>
+                <tbody>
+                   {
+                    vetor.map((obj,indice) => (
                         <tr key={indice}>
-                           
-                            <td>{indice + 1}</td>
-                            <td>{obj.nome}</td>
-                            <td>{obj.marca}</td>
-                            <td><button className="btn btn-success">Selecionar</button></td>
+                           <td>{indice + 1}</td>
+                           <td>{obj.nome}</td>
+                           <td>{obj.marca}</td>
+                           <td><button onClick={() => {selecionar(indice)}} className="btn btn-success">Selecionar</button></td>
                         </tr>
-                         
-                ))
-            }
-            </tbody>
+                    ))
+                   }
+                </tbody>
            
         </table>
     )
